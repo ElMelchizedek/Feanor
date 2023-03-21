f79e2cb21ce15c25feb1f95bc149208ae4ea2183
verl <verlofficial@protonmail.com>
2023-03-21 12:40:29 +1100

    Skeleton ui, initial python script, barebones form

---
+++ app.py
@@ -0,0 +1,16 @@
+import os
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+@app.route("/", methods=("POST", "GET"))
+def index():
+    if request.method == "POST":
+        film = request.form["film"]
+        resonse = film
+        return redirect(url_for("index", result=film))
+    else:
+        result = request.args.get("result")
+        return render_template("index.html", result=result)
